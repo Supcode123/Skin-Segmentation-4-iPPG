e86cf89ec63f04bfa14624ebb2d449aa6b7d32b2
Supcode123 <shikunzhi869@gmail.com>
2025-01-05 00:58:38 +0100

    unet++

--- models/Archi.py
+++ models/Archi.py
@@ -1,12 +1,11 @@
-from models.EfficientNetb0_UNet import model_create
-from models.unet3plus.UNet_3Plus import UNet_3Plus
+from models.EfficientNetb0_UNet.model_create import efficientnetb0_unet
+from models.UNet2Plus.model_create import unet2plus
 
 
 def model_select(model_cfg: dict, num_classes: int = 2):
     if model_cfg['NAME'] == "EfficientNetb0_UNet":
-        return model_create.efficientnetb0_unet(model_cfg, num_classes)
+        return efficientnetb0_unet(model_cfg, num_classes)
 
-    if model_cfg['NAME'] == "Unet3plus":
-        # if num_classes == 2:
-        #     cls = 1
-        return UNet_3Plus(in_channels=3, n_classes=1)
+    if model_cfg['NAME'] == "Unet++":
+
+        return unet2plus(model_cfg)
---
+++ models/UNet2Plus/model_create.py
@@ -0,0 +1,18 @@
+import segmentation_models_pytorch as smp
+
+""" here used the pre-trained Model,which  
+reference to https://github.com/qubvel-org/segmentation_models.pytorch/tree/main"""
+
+
+def unet2plus(model_cfg):
+    backbone = model_cfg['BACKBONE']
+    head = model_cfg['HEAD']
+    model = getattr(smp, head)(
+        encoder_name=backbone,
+        encoder_weights=model_cfg['ENCODER_WEIGHTS'],
+        classes=1,
+        activation=None,
+        encoder_depth=model_cfg['ENCODER_DEPTH'],
+        decoder_channels=model_cfg['DECODER_CHANNELS']
+    )
+    return model
